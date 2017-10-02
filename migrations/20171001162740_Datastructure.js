0c724f624dccb12f586de98bfa4d608c11becfb1
Gregory Demaline <Gregory Demaline>
2017-10-01 21:21:25 -0400

    added tabels to database migration file

--- migrations/20171001162740_Datastructure.js
+++ migrations/20171001162740_Datastructure.js
@@ -2,6 +2,18 @@
 exports.up = function(knex, Promise) {
      return knex
             .schema
+
+            .createTable( 'skills', function( skillsTable ) {
+                // Primary Key
+                skillsTable.increments();
+
+                // Data
+                skillsTable.string( 'skillname', 50 ).notNullable();
+
+                skillsTable.timestamp( 'created_at' ).notNullable();
+
+            })
+
             .createTable( 'users', function( usersTable ) {
 
                 // Primary Key
@@ -9,40 +21,84 @@ exports.up = function(knex, Promise) {
 
                 // Data
                 usersTable.string( 'name', 50 ).notNullable();
-                usersTable.string( 'username', 50 ).notNullable().unique();
                 usersTable.string( 'email', 250 ).notNullable().unique();
                 usersTable.string( 'password', 128 ).notNullable();
-                usersTable.string( 'guid', 50 ).notNullable().unique();
+                usersTable.string( 'racfid', 50 ).notNullable().unique();
+                usersTable.money( 'hourlyrate', 50 ).notNullable().unique();
 
                 usersTable.timestamp( 'created_at' ).notNullable();
 
             } )
 
-            .createTable( 'birds', function( birdsTable ) {
+            .createTable( 'projects', function( projectsTable ) {
 
                 // Primary Key
-                birdsTable.increments();
-                birdsTable.string( 'owner', 36 ).references( 'guid' ).inTable( 'users' );
+                projectsTable.increments();
 
                 // Data
-                // Each chainable method creates a column of the given type with the chained constraints. For example, in the line below, we create a column named `name` which has a maximum length of 250 characters, is of type string (VARCHAR) and is not nullable. 
-                birdsTable.string( 'name', 250 ).notNullable();
-                birdsTable.string( 'species', 250 ).notNullable();
-                birdsTable.string( 'picture_url', 250 ).notNullable();
-                birdsTable.string( 'guid', 36 ).notNullable().unique();
-                birdsTable.boolean( 'isPublic' ).notNullable().defaultTo( true );
+                projectsTable.string( 'projectname', 50 ).notNullable();
+                projectsTable.date( 'startdate', 50 ).notNullable().unique();
+                projectsTable.date( 'deadline', 250 ).notNullable().unique();
+                projectsTable.money( 'totalcost', 128 ).notNullable();
+                projectsTable.string( 'epicbucket', 50 ).notNullable().unique();
+                projectsTable.boolean( 'iscomplete', 50 ).notNullable().unique();
+                projectsTable.boolean( 'isfunded', 50 ).notNullable().unique();
 
-                birdsTable.timestamp( 'created_at' ).notNullable();
+                projectsTable.timestamp( 'created_at' ).notNullable();
+
+            } )
+
+            .createTable( 'projectskills', function( projectskillsTable ) {
+
+                // Primary Key
+                projectskillsTable.increments();
+
+                // Data
+                projectskillsTable.string( 'project', 50 ).references( 'projectname' ).inTable( 'projects' );
+                projectskillsTable.string( 'skill', 50 ).references( 'skillname' ).inTable( 'skills' );               
+
+                projectskillsTable.timestamp( 'created_at' ).notNullable();
+
+            } )
+
+            .createTable( 'userskills', function( userskillsTable ) {
+
+                // Primary Key
+                userskillsTable.increments();
+
+                // Data
+                userskillsTable.string( 'racfid', 50 ).references( 'racfid' ).inTable( 'users' );
+                userskillsTable.string( 'skill', 50 ).references( 'skillname' ).inTable( 'skills' );               
+
+                userskillsTable.timestamp( 'created_at' ).notNullable();
+
+            } )
+
+            .createTable( 'projectusers', function( projectskillsTable ) {
+
+                // Primary Key
+                projectusersTable.increments();
+
+                // Data
+                projectusersTable.string( 'racfid', 50 ).references( 'racfid' ).inTable( 'users' );
+                projectusersTable.string( 'project', 50 ).references( 'projectname' ).inTable( 'projects' );               
+
+                projectusersTable.timestamp( 'created_at' ).notNullable();
 
             } );
 
+            
 };
 
 exports.down = function(knex, Promise) {
       // We use `...ifExists` because we're not sure if the table's there. Honestly, this is just a safety measure. 
     return knex
         .schema
-            .dropTableIfExists( 'birds' )
-            .dropTableIfExists( 'users' );
+            .dropTableIfExists( 'projectusers' )
+            .dropTableIfExists( 'userskills' )
+            .dropTableIfExists( 'projectskills' )
+            .dropTableIfExists( 'skills' )
+            .dropTableIfExists( 'users' )
+            .dropTableIfExists( 'projects' );
   
 };
